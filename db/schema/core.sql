ac577bbd7da219d5a97466d2016821fb64a0ff27
Daniel Blumenthal <daniel@tryscratch.com>
2016-07-09 17:36:59 -0400

    split db roles into a separate file, filled out basic user/group fields

--- db/schema/core.sql
+++ db/schema/core.sql
@@ -1,4 +1,44 @@
 -- core.sql
 -- schema definitions for the core database
 
-CREATE ROLE admin WITH SUPERUSER LOGIN PASSWORD 'foo';
+DROP TABLE IF EXISTS t_user;
+CREATE TABLE t_user (
+  id SERIAL PRIMARY KEY,
+  handle VARCHAR(255),
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255),
+  facebook_id VARCHAR(255)
+);
+CREATE INDEX ON t_user (handle);
+CREATE INDEX ON t_user (email);
+CREATE INDEX ON t_user (facebook_id);
+
+DROP TABLE IF EXISTS t_group;
+CREATE TABLE t_group (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  owner_id INTEGER NOT NULL
+);
+CREATE INDEX ON t_group (name);
+CREATE INDEX ON t_group (owner_id);
+
+DROP TABLE IF EXISTS t_group_user;
+CREATE TABLE t_group_user (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  group_id INTEGER NOT NULL,
+  level INTEGER NOT NULL
+);
+CREATE INDEX ON t_group_user (user_id);
+CREATE INDEX ON t_group_user (group_id);
+
+DROP TABLE IF EXISTS t_user_public_key;
+CREATE TABLE t_user_public_key (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  format VARCHAR(255) NOT NULL,
+  public_key VARCHAR(10000) NOT NULL
+);
+CREATE INDEX ON t_user_public_key (user_id);
---
+++ db/schema/roles.sql
@@ -0,0 +1,4 @@
+-- roles.sql
+-- role definitions
+
+CREATE ROLE admin WITH SUPERUSER LOGIN PASSWORD 'foo';
