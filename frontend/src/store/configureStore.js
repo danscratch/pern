ff5d8de8b74fbeed279fa2d51ba07ae42d0e1a91
Daniel Blumenthal <dandreamsofcoding@gmail.com>
2016-07-14 14:53:17 -0400

    allow different prod/dev store code

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,28 +1,5 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
-import rootReducer from '../reducers';
-import DevTools from '../containers/DevTools'
-
-export default function configureStore(preloadedState) {
-  const store = createStore(
-    rootReducer,
-    preloadedState,
-    compose(
-      applyMiddleware(thunk, createLogger()),
-      DevTools.instrument()
-    )
-  );
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      /* eslint-disable global-require */
-      const nextReducer = require('../reducers').default;
-      /* eslint-enable global-require */
-      store.replaceReducer(nextReducer);
-    });
-  }
-
-  return store;
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod')
+} else {
+  module.exports = require('./configureStore.dev')
 }
