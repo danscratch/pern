57cfe860bcf47580563db6ba26b0693af3ef94df
Daniel Blumenthal <dandreamsofcoding@gmail.com>
2016-07-24 23:34:35 -0400

    renamed user action types variable

--- frontend/src/actions/user.js
+++ frontend/src/actions/user.js
@@ -3,7 +3,7 @@ import { fetchUserByJwt, loginByUsernamePassword, createUser } from '../services
 import { Auth } from '../services/auth';
 
 
-export const UserConstants = {
+export const UserActionTypes = {
   USER_FETCH: 'USER_FETCH',
   USER_FETCH_SUCCESS: 'USER_FETCH_SUCCESS',
   USER_FETCH_FAILURE: 'USER_FETCH_FAILURE',
@@ -22,7 +22,7 @@ export const UserConstants = {
 export function fetchUser() {
   return (dispatch, getState) => {
     dispatch({
-      type: UserConstants.USER_FETCH,
+      type: UserActionTypes.USER_FETCH,
       status: 200,
       userData: null,
     });
@@ -36,14 +36,14 @@ export function fetchUser() {
       })
       .then(userData => {
         dispatch({
-          type: UserConstants.USER_FETCH_SUCCESS,
+          type: UserActionTypes.USER_FETCH_SUCCESS,
           status: statusText,
           userData,
         });
       })
       .catch(err => {
         dispatch({
-          type: UserConstants.USER_FETCH_FAILURE,
+          type: UserActionTypes.USER_FETCH_FAILURE,
           status: statusText,
           userData: null,
         });
@@ -54,14 +54,14 @@ export function fetchUser() {
 export function logout() {
   Auth.logout();
   return {
-    type: UserConstants.USER_LOGOUT,
+    type: UserActionTypes.USER_LOGOUT,
   };
 }
 
 export function login(username, password) {
   return (dispatch, getState) => {
     dispatch({
-      type: UserConstants.USER_LOGIN,
+      type: UserActionTypes.USER_LOGIN,
       status: 200,
       userData: null,
     });
@@ -80,14 +80,14 @@ export function login(username, password) {
     })
     .then(userData => {
       dispatch({
-        type: UserConstants.USER_LOGIN_SUCCESS,
+        type: UserActionTypes.USER_LOGIN_SUCCESS,
         status: statusText,
         userData,
       });
     })
     .catch(err => {
       dispatch({
-        type: UserConstants.USER_LOGIN_FAILURE,
+        type: UserActionTypes.USER_LOGIN_FAILURE,
         status,
         userData: null,
       });
@@ -98,7 +98,7 @@ export function login(username, password) {
 export function signup(userData) {
   return (dispatch, getState) => {
     dispatch({
-      type: UserConstants.USER_CREATE,
+      type: UserActionTypes.USER_CREATE,
       status: 200,
       userData: null,
     });
@@ -117,14 +117,14 @@ export function signup(userData) {
     })
     .then(newUserData => {
       dispatch({
-        type: UserConstants.USER_CREATE_SUCCESS,
+        type: UserActionTypes.USER_CREATE_SUCCESS,
         status: statusText,
         userData: newUserData,
       });
     })
     .catch(err => {
       dispatch({
-        type: UserConstants.USER_CREATE_FAILURE,
+        type: UserActionTypes.USER_CREATE_FAILURE,
         status,
         userData: null,
       });
--- frontend/src/reducers/user.js
+++ frontend/src/reducers/user.js
@@ -1,4 +1,4 @@
-import { UserConstants } from '../actions/user';
+import { UserActionTypes } from '../actions/user';
 
 const initialState = {
   isFetching: false,
@@ -8,27 +8,27 @@ const initialState = {
 
 export default function user(state = initialState, action) {
   switch (action.type) {
-    case UserConstants.USER_LOGOUT:
+    case UserActionTypes.USER_LOGOUT:
       return {
         isFetching: false,
         status: 200,
         userData: null,
       };
 
-    case UserConstants.USER_FETCH:
-    case UserConstants.USER_LOGIN:
-    case UserConstants.USER_CREATE:
+    case UserActionTypes.USER_FETCH:
+    case UserActionTypes.USER_LOGIN:
+    case UserActionTypes.USER_CREATE:
       return {
         isFetching: true,
         userData: null,
       };
 
-    case UserConstants.USER_LOGIN_SUCCESS:
-    case UserConstants.USER_LOGIN_FAILURE:
-    case UserConstants.USER_FETCH_SUCCESS:
-    case UserConstants.USER_FETCH_FAILURE:
-    case UserConstants.USER_CREATE_SUCCESS:
-    case UserConstants.USER_CREATE_FAILURE:
+    case UserActionTypes.USER_LOGIN_SUCCESS:
+    case UserActionTypes.USER_LOGIN_FAILURE:
+    case UserActionTypes.USER_FETCH_SUCCESS:
+    case UserActionTypes.USER_FETCH_FAILURE:
+    case UserActionTypes.USER_CREATE_SUCCESS:
+    case UserActionTypes.USER_CREATE_FAILURE:
       return {
         isFetching: false,
         status: action.status,
